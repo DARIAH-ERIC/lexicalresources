9ebb5fe4bdd738a49c5b5bf54698cdcd72d1706c
Toma Tasovac <ttasovac@humanistika.org>
2022-12-31 16:18:23 +0100

    fix algo

--- docs/pages/TEILex0/js/algo.js
+++ docs/pages/TEILex0/js/algo.js
@@ -1,5 +1,6 @@
 docsearch({
-    apiKey: '27fc9785932247d5056acddcede378cf',
+    apiKey: '81fe7e18f931f5024ff56f875ed7e8fb',
+    appId: 'TFWYQALT5Y',
     indexName: 'tei_lex',
     inputSelector: '.algo',
     debug: true, // Set debug to true if you want to inspect the dropdown
